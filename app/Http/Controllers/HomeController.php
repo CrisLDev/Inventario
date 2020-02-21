a04458a493f4503599e1f0719977e75fb54094e3
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-02-21 12:01:29 -0500

    Agregado carousel de imagenes perfectaente.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Item;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -28,5 +29,10 @@ class HomeController extends Controller
         $items_activos = Item::where('it_activo', '>', '0')->get()->count();
         $items_inactivos = Item::Where('it_Activo', '<=', '0')->get()->count();
         return view('home', compact('items', 'items_activos', 'items_inactivos'));
+    }
+
+    public function welcome(){
+        $posts = Post::all();
+        return view('welcome', compact('posts'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,27 +9,14 @@
           <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="/storage/5UKGSfStAcrs7sa82Ji6LgnJ1DxiWCQp22ga9167.jpeg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="/storage/5UKGSfStAcrs7sa82Ji6LgnJ1DxiWCQp22ga9167.jpeg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="/storage/posts/img/5UKGSfStAcrs7sa82Ji6LgnJ1DxiWCQp22ga9167.jpeg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </div>
-          </div>
+            @foreach ($posts as $post)
+                <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+                    <img src="{{$post->ps_path}}" class="d-block w-100 h-auto" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>{{$post->ps_nombre}}</h5>
+                    </div>
+                </div>
+            @endforeach
         </div>
         <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
