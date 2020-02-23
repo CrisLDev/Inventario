e2098faa98e034cf030515340efca082e4d7157d
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-02-23 00:12:57 -0500

    Obteniendo solo los 3 ultimos post.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
     }
 
     public function welcome(){
-        $posts = Post::all();
+        $posts = Post::limit(3)->latest()->get();
         return view('welcome', compact('posts'));
     }
 }
