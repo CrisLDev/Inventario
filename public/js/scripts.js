f73ba74d594eaa8a161d3453e3e9a126dad658fc
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-03-31 14:49:55 -0500

    Bloqueo de boton

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
 function disable() {
-    document.getElementsByClassName("#button-prevent-multiple-submits").disabled = true;
+    document.getElementsById("button-prevent-multiple-submits").disabled = true;
     document.getElementById("spinner").style.display='inline-block';
     document.getElementById("btex").innerText='Cargando';
 }
